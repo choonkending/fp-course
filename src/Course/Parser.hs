89d86087eebec5a9e2fd52f64b649ceeda48dbed
ken-ding <ken.ding@rea-group.com>
2018-01-31 12:24:46 +1100

    Add sequenceParser

--- src/Course/Parser.hs
+++ src/Course/Parser.hs
@@ -456,7 +456,7 @@ sequenceParser ::
   List (Parser a)
   -> Parser (List a)
 sequenceParser =
-  error "todo: Course.Parser#sequenceParser"
+  foldRight (lift2 (:.)) (pure Nil)
 
 -- | Return a parser that produces the given number of values off the given parser.
 -- This parser fails if the given parser fails in the attempt to produce the given number of values.

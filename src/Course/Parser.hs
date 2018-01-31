b2a2ae096eb6dfdc1c21d6320a4ec5513e4c67ca
ken-ding <ken.ding@rea-group.com>
2018-01-31 14:13:20 +1100

    Add smokeParser

--- src/Course/Parser.hs
+++ src/Course/Parser.hs
@@ -574,7 +574,8 @@ surnameParser =
 smokerParser ::
   Parser Char
 smokerParser =
-  error "todo: Course.Parser#smokerParser"
+  is 'y' ||| is 'n'
+
 
 -- | Write part of a parser for Person#phoneBody.
 -- This parser will only produce a string of digits, dots or hyphens.
